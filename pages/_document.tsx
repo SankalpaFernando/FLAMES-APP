690daab8e4e7815bde95152da86cc210ba5a5e90
Sankalpa <sankalpafernando2017@gmail.com>
2022-04-09 01:55:04 +0530

    Meta Tags Added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,30 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <title>FLAMES</title>
+        <meta
+          name="description"
+          content="Online Tool to Play FLAMES a game that uses to find out the relationship between two people by their names"
+        />
+        <meta property="og:title" content="AlgoViz" />
+        <meta
+          property="og:description"
+          content="Online Tool to Play FLAMES a game that uses to find out the relationship between two people by their names"
+        />
+        <meta
+          property="og:url"
+          content="https://www.flames.sankalpafernando.com/"
+        />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/SankalpaFernando/fileHosting/main/Screenshot%20from%202022-04-09%2001-51-49.png"
+        />
+        <meta property="og:type" content="website" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💕</text></svg>"
+        />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
